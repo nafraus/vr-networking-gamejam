6e9011dd921d2f253855611780cdbf0623402886
nfraus <frausnic@msu.edu>
2024-03-24 14:58:20 -0400

    PlayerScoreUI

---
+++ Assets/_Scripts/ScoreUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text text;
+    [SerializeField] private PlayerScore score;
+    [SerializeField] private float angleThreshhold = 10f;
+
+    bool isInBounds => Vector3.Angle(Camera.main.transform.forward, transform.forward) <= angleThreshhold;
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Vector3.Angle(Camera.main.transform.forward, -transform.up));
+        if (isInBounds) text.color = Color.white;
+        else text.color = Color.clear;
+
+        text.text = score.Score.ToString();
+    }
+
+
+}
